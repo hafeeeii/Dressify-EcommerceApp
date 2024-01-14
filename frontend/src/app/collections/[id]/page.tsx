c6e688f7aa2b55fde7d24bdffe1cae7c6e24ed4c
Hafis <hafisk469@gmail.com>
2024-01-14 11:06:41 +0530

    collection page changed

--- frontend/src/app/collections/[id]/page.tsx
+++ frontend/src/app/collections/[id]/page.tsx
@@ -1,29 +1,29 @@
-// import Card from "@/components/Card";
-// import FilterSection from "@/components/FilterSection";
-// import { useFetch } from "@/lib/hooks/useFetch";
-// import { Collection, Product } from "@/lib/utils/types";
-// import React from "react";
+import Card from "@/components/Card";
+import FilterSection from "@/components/FilterSection";
+import { useFetch } from "@/lib/hooks/useFetch";
+import { Collection, Product } from "@/lib/utils/types";
+import React from "react";
 
 
-// const page = async ({ params }: { params: { id: string } }) => {
-//   const newId: number = parseInt(params?.id);
-//   const collectionsData = await useFetch(
-//     `/sub-categories?populate=*&[filters][id][$eq]=${newId}`
-//   );
-//  const mapCollections:Collection = await collectionsData?.data.map((collection:Collection) =>collection)
-//  console.log(mapCollections),'this is m applying';
-//   return (
-//     <div className="min-h-screen w-[100vw]">
-//       <div className="flex flex-col sm:flex-row">
-//         <FilterSection />
-//         <div className=" ">
-//   {mapCollections?.attributes?.products?.data?.map((product:Product)=>{
-//     <Card data={product}/>
-//   })}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const page = async ({ params }: { params: { id: string } }) => {
+  const newId: number = parseInt(params?.id);
+  const collectionsData = await useFetch(
+    `/sub-categories?populate=*&[filters][id][$eq]=${newId}`
+  );
+ const mapCollections:Collection = await collectionsData?.data.map((collection:Collection) =>collection)
+ console.log(mapCollections),'this is m applying';
+  return (
+    <div className="min-h-screen w-[100vw]">
+      <div className="flex flex-col sm:flex-row">
+        <FilterSection />
+        <div className=" ">
+  {mapCollections?.attributes?.products?.data?.map((product:Product)=>{
+    <Card data={product}/>
+  })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default page;
+export default page;
